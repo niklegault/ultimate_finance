3319345d0aae6e62ebfe2b9fe688175f2cff1cc6
Nik Legault <50716413+niklegault@users.noreply.github.com>
2025-08-07 14:00:52 -0400

    added period selector to dashboard screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:ultimate_finance/theme/app_theme.dart';
+import 'package:ultimate_finance/widgets/period_selector.dart';
 
-class DashboardScreen extends StatelessWidget {
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
 
   @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  DateTime _currentPeriod = DateTime.now();
+
+  void _handlePeriodChange(DateTime? newPeriod) {
+    // Handle period change logic here
+    setState(() {
+      _currentPeriod = newPeriod ?? DateTime.now();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Welcome to the Dashboard Screen!'));
+    final financialTheme =
+        Theme.of(context).extension<FinancialThemeExtension>();
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8, top: 48, bottom: 8),
+          child: PeriodSelector(
+            selectedPeriod: _currentPeriod,
+            onPeriodChanged: _handlePeriodChange,
+          ),
+        ),
+        const Center(child: Text('Welcome to the Dashboard Screen!')),
+      ],
+    );
   }
 }
